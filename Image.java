94594230ffa17b783cd0c694695b0d72e52596ec
aj8uppal <aj8uppal@gmail.com>
2014-02-19 19:05:01 -0800

    Image.java
    File for compiling all of the images

---
+++ Image.java
@@ -0,0 +1,11 @@
+import acm.graphics.GImage;
+
+public class Images extends GImage{
+
+	public Images(String string, double x, double y) {
+		super(string, x, y);
+		// TODO Auto-generated constructor stub
+	}
+}
+
+//super("tube.png", 16, 81);
